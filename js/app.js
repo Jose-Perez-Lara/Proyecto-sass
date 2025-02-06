16d57a656715ff51275143bbd850ceda14e9d28d
unknown <2DAW-joseper481@IESMARTINEZM.ES>
2025-02-06 13:05:27 +0100

    Subida 3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const productos = document.querySelectorAll(".producto");
 
 productos.forEach((producto) => {
+  if (producto.classList.contains("expandido")) {
+    producto.querySelector("p").style.display = "inline-block";
+  } else {
+    producto.querySelector("p").style.display = "none";
+  }
   producto.addEventListener("click", () => {
     productos.forEach((p) => {
       p.classList.remove("expandido");
+      p.querySelector("p").style.display = "none";
       if (p.querySelector(".button-compra")) {
         p.querySelector(".button-compra").remove();
       }
     });
     producto.classList.add("expandido");
+    producto.querySelector("p").style.display = "inline-block";
     let button = document.createElement("button");
     button.classList.add("button-compra");
     button.innerText = "Comprar";
